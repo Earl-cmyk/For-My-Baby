8fbcef9cea99efc8ebd9817a162b01df3899daea
Earl-cmyk <erlinadimla@gmail.com>
2025-10-14 22:16:53 +0800

    Added 1-month letter and improved visuals

--- static/script.js
+++ static/script.js
@@ -16,3 +16,12 @@ function createHeart() {
   setTimeout(() => heart.remove(), 7000);
 }
 setInterval(createHeart, 600);
+
+window.addEventListener('scroll', function() {
+  const second = document.querySelector('.second');
+  const rect = second.getBoundingClientRect();
+
+  if (rect.top < window.innerHeight - 150) {
+    second.classList.add('visible');
+  }
+});
